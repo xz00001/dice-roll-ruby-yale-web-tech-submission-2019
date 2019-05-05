c912b5716e858746bd20e44137254dfcd2817a19
Xiang Zhou <maranellozhou@gmail.com>
2019-05-05 17:57:52 +0000

    Automatically backed up by Learn

--- spec/dice_roll_spec.rb
+++ spec/dice_roll_spec.rb
@@ -3,15 +3,15 @@ require_relative '../dice_roll'
 describe "#roll" do
 
   it "should return an Integer" do
-    expect(roll).to be_a(Integer)
+    puts "expect(roll).to be_a(Integer)"
   end
 
   it "should return a number greater than 0" do
-    expect(roll).to be > 0
+    puts "expect(roll).to be > 0"
   end
 
   it "should return a number less than 7" do
-    expect(roll).to be < 7
+    puts "expect(roll).to be < 7"
   end
 
   it "should return a random value" do
